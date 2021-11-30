a31dce07a56ab33d65313a8a28b9d662752b689c
rammess <95316171+rammess@users.noreply.github.com>
2021-11-30 17:09:05 -0600

    commiting to new branch

--- starter/src/main/java/com/ramesh/SessionLogFilter.java
+++ starter/src/main/java/com/ramesh/SessionLogFilter.java
@@ -19,7 +19,7 @@ public class SessionLogFilter implements Filter {
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		//WebApplicationContextUtils.getRequiredWebApplicationContext(filterConfig.getServletContext());
-		//Test appliation now
+		//Test appliation now to create new branch
 	}
 
 	@Override
